da9798292d19a0157637fadc716cebd36eebe388
Rich <rfspencer@users.noreply.github.com>
2024-03-31 12:19:07 -0700

    Add the physics system

---
+++ LightYearsEngine/include/framework/PhysicsSystem.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "framework/Core.h"
+#include "box2d/b2_world.h"
+
+namespace ly
+{
+    class PhysicsSystem
+    {
+    public:
+        static PhysicsSystem& Get();
+
+    protected:
+        PhysicsSystem();
+
+    private:
+        static unique<PhysicsSystem> physicsSystem;
+
+        b2World mPhysicsWorld;
+        float mPhysicsScale;
+    };
+}
