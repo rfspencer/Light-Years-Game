33507af546d01e383e30ae64958c66d22f652197
Rich <rfspencer@users.noreply.github.com>
2024-03-31 15:03:45 -0700

    Enable Get Weak Pointer from This

--- LightYearsEngine/include/framework/Object.h
+++ LightYearsEngine/include/framework/Object.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <memory>
+#include "Core.h"
+
 namespace ly
 {
-    class Object
+class Object : public std::enable_shared_from_this<Object>
     {
     public:
         Object();
@@ -10,6 +13,9 @@ namespace ly
 
         virtual void Destroy();
         bool IsPendingDestroy() const { return mIsPendingDestroy; }
+
+        weak<Object> GetWeakRef();
+        weak<const Object> GetWeakRef() const;
 
     private:
         bool mIsPendingDestroy;
--- LightYearsEngine/src/framework/Object.cpp
+++ LightYearsEngine/src/framework/Object.cpp
@@ -17,4 +17,14 @@ namespace ly
     {
         mIsPendingDestroy = true;
     }
+
+    weak<Object> Object::GetWeakRef()
+    {
+        return weak_from_this();
+    }
+
+    weak<const Object> Object::GetWeakRef() const
+    {
+        return weak_from_this();
+    }
 }
