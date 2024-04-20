893ff7d0f3c1691b1cebb3c510ea6015b5f1ca08
Rich <rfspencer@users.noreply.github.com>
2024-04-20 11:39:00 -0700

    Create the Base Game Stage Class

---
+++ LightYearsEngine/src/gameplay/GameStage.cpp
@@ -0,0 +1,35 @@
+#include "gameplay/GameStage.h"
+#include "framework/Core.h"
+
+namespace ly
+{
+
+    GameStage::GameStage(World *world)
+        : mWorld{world},
+        mStageFinished{false}
+    {
+
+    }
+
+    void GameStage::StartStage()
+    {
+        LY_LOG("Stage Started");
+    }
+
+    void GameStage::TickStage(float deltaTime)
+    {
+        LY_LOG("Stage Ticking");
+    }
+
+    void GameStage::FinishStage()
+    {
+        onStageFinished.Broadcast();
+        mStageFinished = true;
+        StageFinished();
+    }
+
+    void GameStage::StageFinished()
+    {
+        LY_LOG("Stage Finished");
+    }
+}
