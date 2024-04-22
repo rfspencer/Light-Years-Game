5ed04479781f911d1dbfa15688bf7e86dd37c8b0
Rich <rfspencer@users.noreply.github.com>
2024-04-21 17:41:33 -0700

    Add the Player Class

---
+++ LightYearsGame/include/player/Player.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "framework/Object.h"
+#include "framework/Delegate.h"
+
+namespace ly
+{
+    class PlayerSpaceShip;
+    class World;
+
+    class Player : public Object
+    {
+    public:
+        Player();
+
+        weak<PlayerSpaceShip> SpawnSpaceShip(World* world);
+        weak<PlayerSpaceShip> GetCurrentSpaceShip() const { return mCurrentPlayerSpaceShip; }
+
+        void AddLifeCount(unsigned int count);
+        int GetLifeCount() const { return mLifeCount; }
+
+        int AddScore(unsigned int amount);
+        int GetScore() const { return mScore; }
+
+        Delegate<int> onLifeChanged;
+        Delegate<int> onScoreChange;
+        Delegate<> onLifeExhausted;
+
+    private:
+        unsigned int mLifeCount;
+        unsigned int mScore;
+        weak<PlayerSpaceShip> mCurrentPlayerSpaceShip;
+    };
+}
---
+++ LightYearsGame/src/player/Player.cpp
@@ -0,0 +1,52 @@
+
+#include "player/Player.h"
+
+#include "framework/World.h"
+#include "player/PlayerSpaceShip.h"
+
+namespace ly
+{
+
+    Player::Player()
+        : mLifeCount{3},
+        mScore{0},
+        mCurrentPlayerSpaceShip{}
+    {
+
+    }
+
+    weak<PlayerSpaceShip> Player::SpawnSpaceShip(World *world)
+    {
+        if (mLifeCount > 0)
+        {
+            --mLifeCount;
+            auto windowSize = world->GetWindowSize();
+            mCurrentPlayerSpaceShip = world->SpawnActor<PlayerSpaceShip>();
+            mCurrentPlayerSpaceShip.lock()->SetActorLocation(sf::Vector2f(windowSize.x / 2.f, windowSize.y - 100.f));
+            mCurrentPlayerSpaceShip.lock()->SetActorRotation(0.f);
+            onLifeChanged.Broadcast(mLifeCount);
+        }
+        else
+        {
+            onLifeExhausted.Broadcast();
+        }
+    }
+
+    void Player::AddLifeCount(unsigned int count)
+    {
+        if (count > 0)
+        {
+            mLifeCount += count;
+            onLifeChanged.Broadcast(mLifeCount);
+        }
+    }
+
+    int Player::AddScore(unsigned int amount)
+    {
+        if (amount > 0 )
+        {
+            mScore += amount;
+            onScoreChange.Broadcast(mScore);
+        }
+    }
+}
