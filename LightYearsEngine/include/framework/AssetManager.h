06d4c5925f8fa5ea215d19dd2fbf3a257b6dfa4f
Rich <rfspencer@users.noreply.github.com>
2024-04-24 19:27:24 -0700

    Refactor the Asset Manager

--- LightYearsEngine/include/framework/AssetManager.h
+++ LightYearsEngine/include/framework/AssetManager.h
@@ -18,9 +18,51 @@ namespace ly
         AssetManager();
 
     private:
+        template<typename T>
+        shared<T> LoadAsset(const std::string& path, Dictionary<std::string, shared<T>>& container);
+
+        template<typename T>
+        void CleanUniqueRef(Dictionary<std::string, shared<T>>& container);
+
         static unique<AssetManager> assetManager;
         Dictionary<std::string, shared<sf::Texture>> mLoadedTextureMap;
         Dictionary<std::string, shared<sf::Font>> mLoadedFontMap;
         std::string mRootDirectory;
     };
+
+    template<typename T>
+    inline shared<T> AssetManager::LoadAsset(const std::string &path, Dictionary<std::string, shared<T>> &container)
+    {
+        auto found = container.find(path);
+        if(found != container.end())
+        {
+            return found->second;
+        }
+
+        shared<T> newAsset{new T};
+        if (newAsset->loadFromFile(mRootDirectory + path))
+        {
+            container.insert({path, newAsset});
+            return newAsset;
+        }
+
+        return shared<T> {nullptr};
+    }
+
+    template<typename T>
+    inline void AssetManager::CleanUniqueRef(Dictionary<std::string, shared<T>>& container)
+    {
+        for(auto iter = container.begin(); iter != container.end();)
+        {
+            if(iter->second.unique())
+            {
+                LY_LOG("Cleaning: %s", iter->first.c_str());
+                iter = container.erase(iter);
+            }
+            else
+            {
+                ++iter;
+            }
+        }
+    }
 }
